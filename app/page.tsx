f45810ccea693728330a51234d335bd7a464c0e2
Magda <auroralivee1@gmail.com>
2026-05-29 20:54:51 -0500

    Primer commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+  'use client';
+
+export default function Home() {
+return ( <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center"> <h1 className="text-5xl font-bold text-pink-500">
+AURA FIT </h1>
+  <p className="mt-4 text-zinc-400">
+    Tu app fitness femenina 💖
+  </p>
+
+  <button className="mt-6 bg-pink-500 px-6 py-3 rounded-2xl font-semibold">
+    Empezar
+  </button>
+</div>
+);
+}
